923e6e08c23d33963c87eb2a72ffae75eed3b345
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-15 09:37:56 +0900

    [FIX] name 변경

--- hotel/src/main/java/Main.java
+++ hotel/src/main/java/Main.java
@@ -3,11 +3,17 @@ import java.util.Arrays;
 
 public class Main {
 
-    available
-    static void daysGo (ArrayList<Integer> availableRoomList) {
-        for (int t = 0; t < availableRoomList.size(); t++) {
-            if (availableRoomList.get(t) == 0) continue;
-            availableRoomList.set(t, availableRoomList.get(t) - 1);
+    //함수는 동작을 의미한다
+    //함수명은 동작을 표현해주어야 한다
+    //동사가 먼저 나오는 이름을 권장한다
+    //일수가 감수 / 추가할 경우
+
+    static void decreaseDays (ArrayList<Integer> remainingDays) {
+        for (int i = 0; i < remainingDays.size(); i++) {
+            //주석으로 작성하는 것보다 그냥 변수명으로 작성하기
+            int remainingDay = remainingDays.get(i);
+            if (remainingDay == 0) continue;
+            remainingDays.set(i, remainingDay - 1);
         }
     }
 
